ca528705d7aad25414b342e38d06770895c5a3e8
Lisa Chan <xlisachan@gmail.com>
2018-02-15 18:18:47 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist = {};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  var playlist = {artistName: songTitle};
+}
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+}
